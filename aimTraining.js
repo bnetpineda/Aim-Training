aa41a46425800935da9e2cb24d6529898c8e264d
Mark Pineda <markbennettpineda@gmail.com>
2023-10-28 22:41:17 +0800

    80% Complete

--- aimTraining.js
+++ aimTraining.js
@@ -1,24 +1,126 @@
 let points = 0;
-      document.getElementById("points").textContent = "Points: " + points;
+let difficulty,
+  gameMode,
+  start = false,
+  time = 5;
 
-      function changePosition() {
-        const myElement = document.getElementById("button");
-        const randomInt = Math.floor(Math.random() * 80) + 10;
-        myElement.style.left = randomInt + "%";
+function buttonMode(gameMode) {
+  if (gameMode === "horizontalShot") {
+    gameMode = "horizontalShot";
+  } else if (gameMode === "spiderShot") {
+    gameMode = "spiderShot";
+  }
+  document
+    .querySelectorAll(".button")
+    .forEach((buttonMode) => buttonMode.remove());
 
-        const imgElement = document.createElement("img");
-        imgElement.src = "images/dot.png";
-        myElement.innerHTML = "";
-        myElement.appendChild(imgElement);
-      }
+  const textElement = document.getElementById("textRemove");
+  if (textElement) {
+    textElement.innerHTML = "";
+  }
+}
 
-      function click() {
-        const buttonElement = document.getElementById("button");
-        buttonElement.addEventListener("click", function () {
-          points += 1;
-          document.getElementById("points").textContent = "Points: " + points;
-          document.getElementById("points").textContent = "Points: " + points;
-        });
-      }
-      setInterval(changePosition, 1000);
-      click();
+function button(difficulty) {
+  let spiderTrue = false;
+
+  if (difficulty.includes("Spider")) {
+    spiderTrue = true;
+  }
+  if (difficulty === "easy" || difficulty === "easySpider") {
+    difficulty = 3;
+  } else if (difficulty === "normal" || difficulty === "normalSpider") {
+    difficulty = 2;
+  } else if (difficulty === "hard" || difficulty === "hardSpider") {
+    difficulty = 1;
+  }
+  document.getElementById("points").textContent = "Points: " + points;
+
+  document.querySelectorAll(".button").forEach((button) => button.remove());
+
+  const textElement = document.getElementById("textRemove");
+  if (textElement) {
+    textElement.innerHTML = "";
+  }
+
+  if (spiderTrue == true) {
+    setInterval(function () {
+      spideraddImage(difficulty);
+    }, difficulty * 800);
+    startTimer();
+  } else {
+    setInterval(function () {
+      addImage(difficulty);
+    }, difficulty * 800);
+    startTimer();
+  }
+}
+
+function addImage(difficulty) {
+  let randNum = Math.floor(Math.random() * 80) + 10;
+  document.getElementById("image-container").style.left = randNum + "%";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.animation =
+    "fadeOut " + difficulty * 800 + "ms forwards";
+  setTimeout(() => {
+    document.getElementById("image").style.display = "none";
+  }, 750 * difficulty);
+}
+function spideraddImage(difficulty) {
+  let randNum = Math.floor(Math.random() * 80) + 10;
+  let randNum1 = Math.floor(Math.random() * 80) + 10;
+  document.getElementById("image-container").style.left = randNum + "%";
+  document.getElementById("image-container").style.top = randNum1 + "%";
+  document.getElementById("image").style.display = "block";
+  document.getElementById("image").style.animation =
+    "fadeOut " + difficulty * 800 + "ms forwards";
+  setTimeout(() => {
+    document.getElementById("image").style.display = "none";
+  }, 750 * difficulty);
+}
+function updateTimer() {
+  const timerElement = document.getElementById("timer");
+  timerElement.textContent = time;
+}
+function totalPoints() {
+  let stringScore;
+  if (points < 20){
+    stringScore = "Failed"
+  }else if(points > 20 && points <= 40){
+    stringScore = "Good"
+  }else if (points > 40){
+    stringScore = "Excellent"
+  }
+  document.getElementById("totalScore").textContent = "A score of " + points + " is " + stringScore
+  document.getElementById("totalScore").style.animation =
+    "fadeIn 5s forwards";
+}
+function startTimer() {
+  const countdownInterval = setInterval(function () {
+    time -= 1;
+    updateTimer();
+
+    if (time === 0) {
+      clearInterval(countdownInterval);
+      totalPoints();
+      setTimeout(function() {
+        window.location.href = "index.html";
+      }, 5000);
+    }
+  }, 1000);
+}
+
+if (time === 0) {
+  clearInterval(addImage);
+}
+
+function removeImage() {
+  document.getElementById("image").style.display = "none";
+  points += 1;
+  document.getElementById("points").textContent = "Points: " + points;
+  updateTimer();
+}
+
+document.getElementById("image").addEventListener("click", removeImage);
+document.getElementById("goBackButton").addEventListener("click", function () {
+  window.history.back();
+});
